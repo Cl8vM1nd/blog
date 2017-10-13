a1d70629d1d2d97e40425220fb9bf473dd859592
Ilya Faist <ilyaf@scholarshipowl.com>
2017-10-13 20:24:00 +0300

    Change chmod

--- up.sh
+++ up.sh
@@ -15,9 +15,10 @@ echo '1.Artisan'
 docker exec -it blog php artisan migrate
 echo '2.Composer'
 docker exec -it blog composer install
-echo '3.Bower'
-docker exec -it blog bower update --allow-root
-echo '4.Chown'
+#echo '3.Bower'
+#docker exec -it blog bower update --allow-root
+echo '3.Chown'
 docker exec -it blog chown -R www-data:www-data /var/www
-echo '5.Chmod'
-docker exec -it blog chmod -R 777 /var/www/blog/storage && chmod -R 777 /var/www/blog/bootstrap/cache
+echo '4.Chmod'
+docker exec -it blog chmod -R 777 /var/www/blog/storage
+docker exec -it blog chmod -R 777 /var/www/blog/bootstrap/cache
