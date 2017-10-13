4217183331493005cf43b38c0a587b8fffec5009
Ilya Faist <ilyaf@scholarshipowl.com>
2017-10-13 18:27:33 +0300

    Change image version

--- up.sh
+++ up.sh
@@ -11,7 +11,13 @@ docker-compose \
   up -d
 
 cp website/.env.production website/.env
+echo '1.Artisan'
 docker exec -it blog php artisan migrate
+echo '2.Composer'
 docker exec -it blog composer install
+echo '3.Bower'
 docker exec -it blog bower update --allow-root
+echo '4.Chown'
 docker exec -it blog chown -R www-data:www-data /var/www
+echo '5.Chmod'
+docker exec -it blog chmod -R 777 /var/www/blog/storage && chmod -R 777 /var/www/blog/bootstrap/cache
