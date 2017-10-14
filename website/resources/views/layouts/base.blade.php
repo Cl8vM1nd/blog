6084ed7923e7ac504b8df3486294147264c553a7
Ilya Faist <ilyaf@scholarshipowl.com>
2017-10-14 23:19:57 +0300

    Add GA

--- website/resources/views/layouts/base.blade.php
+++ website/resources/views/layouts/base.blade.php
@@ -10,6 +10,15 @@
     <link rel="stylesheet" href="/vendor/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="/vendor/iconmonstr-iconic-font/css/iconmonstr-iconic-font.min.css">
     <link rel="stylesheet" href="/css/main.css">
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108094573-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-108094573-1');
+    </script>
 </head>
 <body>
 @inject('ajax', 'App\Services\AjaxAuthService')
