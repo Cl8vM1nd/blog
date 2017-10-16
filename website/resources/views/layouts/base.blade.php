89b6c50e872d08c892d53d688e398d3ec7db0ce7
Ilya Faist <ilyaf@scholarshipowl.com>
2017-10-16 06:40:52 +0300

    Add google tag manager

--- website/resources/views/layouts/base.blade.php
+++ website/resources/views/layouts/base.blade.php
@@ -10,17 +10,15 @@
     <link rel="stylesheet" href="/vendor/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="/vendor/iconmonstr-iconic-font/css/iconmonstr-iconic-font.min.css">
     <link rel="stylesheet" href="/css/main.css">
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108094573-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-108094573-1');
-    </script>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-K25LZ78');</script>
 </head>
 <body>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K25LZ78"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 @inject('ajax', 'App\Services\AjaxAuthService')
 @include('index.header')
     <div class="row">
